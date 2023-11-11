25f1a08fcdc7af0679c790f8e7a9f43fddbc95f3
Bimsara Walallawita <bwalallawita@gmail.com>
2023-11-11 12:58:16 +0530

    API requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import SingleProduct from './pages/SingleProduct';
+import Category from './pages/Category';
 
 const App = () => { //Main Component
   return (
@@ -13,6 +14,7 @@ const App = () => { //Main Component
         <Route index element={<Home />} />
         <Route path="/products" element={<Product />} />
         <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/categories/:id" element={<Category />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,17 @@ import { Link } from "react-router-dom";
 const Home = () => {
 
     const [products, setProducts] = useState(null);
+    const [categories, setCategories] = useState(null);
+
+    const [name, setName] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [qty, setQty] = useState(0);
+    const [categoryId, setCategoryId] = useState(null);
 
     useEffect(() => {
-        getProducts();   
-    },[])
+        getProducts();
+        getCategories();
+    }, [])
 
     const getProducts = () => {
         fetch("http://localhost:8081/products")
@@ -22,8 +29,82 @@ const Home = () => {
             })
     }
 
+    const getCategories = () => {
+        fetch("http://localhost:8081/categories")
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                setCategories(data);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const handleName = (event) => {
+        setName(event.target.value);
+    }
+
+    const handlePrice = (event) => {
+        setPrice(event.target.value);
+    }
+
+    const handleQty = (event) => {
+        setQty(event.target.value);
+    }
+
+    const handleCategory = (event) => {
+        setCategoryId(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            "name": name,
+            "price": price,
+            "qty": qty,
+            "categoryId": categoryId
+        }
+
+        fetch("http://localhost:8081/products", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setProducts([...products, data]); //using Spread Operator a,b,c,d ,e
+            setName(null);
+            setPrice(null);
+            setQty(null);
+            setCategoryId(null);
+
+            console.log(products)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
         <>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            {categories && categories.map((category) => (
+                                <li class="nav-item">
+                                    <Link to={`/categories/${category.id}`} className="nav-link">{category.name}</Link>
+                                </li>
+                            ))}
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
             <h1>Home</h1>
 
             <ul>
@@ -41,6 +122,34 @@ const Home = () => {
                     </li>
                 ))}
             </ol>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Product Name</label>
+                    <input type="text" required className="form-control" onChange={handleName} value={name} />
+                </div>
+                <div>
+                    <label>Product Price</label>
+                    <input type="text" required onChange={handlePrice} value={price} />
+                </div>
+                <div>
+                    <label>Product Qty</label>
+                    <input type="text" required onChange={handleQty} value={qty} />
+                </div>
+                <div>
+                    <label>Category</label>
+                    <select required onChange={handleCategory}>
+                        <option>Please Select</option>
+
+                        {categories && categories.map((category) => (
+                            <option key={category.id} value={category.id}>{category.name}</option>
+                        ))}
+
+                    </select>
+                </div>
+
+                <button className="btn btn-primary" type="submit">Save Product</button>
+            </form>
 
         </>
     )
